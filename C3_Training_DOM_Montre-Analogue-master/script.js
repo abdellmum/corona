501af459a5284bde01767950fbc2f4ef5dd7b352
abdellmum <karimovich75@gmail.com>
2020-04-15 01:16:45 +0100

    added choron hour

--- C3_Training_DOM_Montre-Analogue-master/script.js
+++ C3_Training_DOM_Montre-Analogue-master/script.js
@@ -19,17 +19,32 @@ function clock() {
     AIGUILLEHR.style.transform = 'rotate(' + hoursRatio + 'deg)';
     AIGUILLEMIN.style.transform = 'rotate(' + minutesRatio + 'deg)';
     AIGUILLESEC.style.transform = 'rotate(' + secondsRatio + 'deg)';
-    function playSound() {
-        const tick = new Audio('sound/R.m4a');
-        if (seconds <= 59) {
-            tick.play();
-        }
-    }
-    playSound();
+ 
+    
 };
+ 
+
+function kimHour(){
+var element = document.getElementById('kim-hour');
+var date = new Date ();
+var heure=date.getHours();
+var minute=date.getMinutes();
+var secound=date.getSeconds();
+
+    
+    if (minute < 10) { minute = "0" + minute }
+    
+    if (secound < 10) { secound = "0" + secound }
+    if (heure < 10) { heure = "0" + heure}
+    element.innerHTML = heure + ":" + minute + ":" + secound;
+
+}
 
 
 
 
 
+
+
+setInterval(kimHour,clock, 1000);
 setInterval(clock, 1000);
